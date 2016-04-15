18913d44b055926592a7f5d19b1957a70d5eabd7
TravisIngram <ingramtravis@gmail.com>
2016-04-15 13:01:34 -0500

    Change from scope. to controller as

--- client/client.js
+++ client/client.js
@@ -1,13 +1,30 @@
 var app = angular.module("greenApp", []);
 
-app.controller("GreenController", ["$scope", "$http", function($scope, $http) {
-  $scope.usersList = [];
-  $scope.getInfo = function(){
+// app.controller("GreenController", ["$scope", "$http", function($scope, $http) {
+//   $scope.usersList = [];
+//   $scope.getInfo = function(){
+//     // console.log("click");
+//     $http.get("/create").then(function(response) {
+//     console.log('data for /create',response.data);
+//     $scope.usersList = [];
+//     $scope.usersList = response.data;
+//       });
+//    };
+// }])
+
+// Swap in `controller - as` syntax
+
+  app.controller("GreenController", ["$http", function($http) {
+  	var person = this;
+
+  	person.usersList = [];
+
+  	person.getInfo = function(){
     // console.log("click");
-    $http.get("/create").then(function(response) {
-    console.log('data for /create',response.data);
-    $scope.usersList = [];
-    $scope.usersList = response.data;
+    	$http.get("/create").then(function(response) {
+    		console.log('data for /create',response.data);
+    		person.usersList = [];
+    		person.usersList = response.data;
       });
    };
-}])
+}]);
